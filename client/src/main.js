244f0870f4f741db8f1d2e19c841530922957b01
unknown <liangwei@changingedu.com>
2019-11-22 18:00:31 +0800

    kuangjia

--- client/src/main.js
+++ client/src/main.js
@@ -3,13 +3,26 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import store from "./store"
+import el from "element-ui/src/locale/lang/el";
 
+//设置反向代理
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8421/api'
+axios.defaults.withCredentials = true
+
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
