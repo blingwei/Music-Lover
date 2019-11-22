6f2070ee386db43e70d7cbc3875c8533cb91d27c
unknown <liangwei@changingedu.com>
2019-11-22 17:25:47 +0800

    first commit

---
+++ servic/src/main/java/com/blingwei/musicService/ServicApplication.java
@@ -0,0 +1,13 @@
+package com.blingwei.musicService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicApplication.class, args);
+    }
+
+}
---
+++ servic/src/test/java/com/blingwei/musicService/ServicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blingwei.musicService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
